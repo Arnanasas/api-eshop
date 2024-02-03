5427f16cc129d8ad452ce8dd9714db3ed3f2260a
Arnanasas <arnas.bulk@gmail.com>
2024-02-03 21:28:25 +0200

    fix UpdatedAt

--- services/ProductService.js
+++ services/ProductService.js
@@ -3,10 +3,10 @@ const Product = require("../models/Product"); // Assuming your model is set up
 exports.updateOrCreateProduct = async (products) => {
   try {
     for (const product of products) {
-      const { PID, updatedAt } = product;
+      const { PID, UpdatedAt } = product;
       await Product.findOneAndUpdate(
         { PID: PID },
-        { $set: { updatedAt: updatedAt } },
+        { $set: { updatedAt: UpdatedAt } },
         { upsert: true, new: true }
       );
     }
